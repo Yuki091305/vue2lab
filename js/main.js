ce89d5ab3e7d9a0e491dd651a0d79340731cbab2
Yuki091305 <yukifieri@gmail.com>
2026-02-11 15:51:01 +0700

    у первой таблички можно вписать заголовок

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 new vue({
     el:'#app',
     data:{
+        newtitle: " ",
         columns:{
             column1:[],
             column2:[],
             column3:[],
         },
     },
+    methods:{
+        addCard(){
+            if(this.newTitle.trim() && this.columns.column1.length < 3){
+                this.columns.column1.push({
+                    id: Date.now(),
+                    title: this.newTitle,
+                    items: []
+                });
+                this.newTitle = " ";
+            }
+        }
+    }
 })
