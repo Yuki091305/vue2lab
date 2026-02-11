4012da5dcc4742ab2ee42085c3fc7894b5deaa1a
Yuki091305 <yukifieri@gmail.com>
2026-02-11 18:42:44 +0700

    Второй столбец половина

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
-new vue({
-    el:'#app',
-    data:{
-        newtitle: " ",
-        columns:{
-            column1:[],
-            column2:[],
-            column3:[],
-        },
+let app = new Vue({
+    el: '#app',
+    data: {
+        newTitle: '',
+        newItem: '',
+        columns: {
+            column1: [],
+            column2: [],
+            column3: []
+        }
     },
-    methods:{
-        addCard(){
-            if(this.newTitle.trim() && this.columns.column1.length < 3){
+    methods: {
+        addCard() {
+            if (this.newTitle.trim() && this.columns.column1.length < 3) {
                 this.columns.column1.push({
                     id: Date.now(),
                     title: this.newTitle,
                     items: []
                 });
-                this.newTitle = " ";
+                this.newTitle = '';
+            }
+        },
+        addListItem(card) {
+            if (this.newItem.trim()) {
+                card.items.push({
+                    text: this.newItem,
+                    done: false
+                });
+                this.newItem = '';
             }
         }
     }
-})
+});
